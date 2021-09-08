8e6115dd842a1a5a547448344d843e06563b119f
David Jay <djay8400@gmail.com>
2021-09-08 11:33:44 -0500

    mostly completed search and present day functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,64 @@
-// var repoList = document.querySelector('ul');
-// var fetchButton = document.getElementById('fetch-button');
+var today = new Date();
+var day = today.getDate();
+var month = today.getMonth();
+var year = today.getFullYear()
+var date = month + "/" + day + "/" + year
+var currentDate = document.getElementById('searchDate');
+var currentDate = date;
+var city = document.getElementById('currentCity');
 
-// function getApi() {
-//   // replace `octocat` with anyone else's GitHub username
-//   var requestUrl = 'https://api.github.com/users/octocat/repos';
+//var searchButton = document.getElementsByID('searchBtn');
+    
 
-//   fetch(requestUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       for (var i = 0; i < data.length; i++) {
-//         var listItem = document.createElement('li');
-//         listItem.textContent = data[i].html_url;
-//         repoList.appendChild(listItem);
-//       }
-//     });
-// }
+function getSearchValue() {
+    var inputValue = document.getElementById('field').value;
+    
+    inputValue = inputValue.replace(/\s+/g, '-').toLowerCase();
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + inputValue + '&units=imperial&appid=7469b383f606343a74b5e386f6d67047';
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+            })
+        .then(function (data) {
+            var latitude = data.coord.lat
+            var longitude = data.coord.lon
+            var name = document.createElement('li');
+            name.textContent = data.name;
+            city.appendChild(name);
 
-// fetchButton.addEventListener('click', getApi);
+            var newUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&units=imperial&appid=7469b383f606343a74b5e386f6d67047';
+            fetch(newUrl)
+                .then(function (response) {
+                    return response.json();
+                    })
+                .then(function (data) {
+                    console.log(data)
+                
+                    var temperature = document.createElement('li');
+                    temperature.textContent = 'Temp: ' + data.current.temp + '°F';
+                    city.appendChild(temperature);
+                
+                    var listItem = document.createElement('li');
+                    listItem.textContent = 'Wind: ' + data.current.wind_speed + ' MPH';
+                    city.appendChild(listItem);
+
+                    var listItem = document.createElement('li');
+                    listItem.textContent = 'Humidity: ' + data.current.humidity + ' %';
+                    city.appendChild(listItem);
+
+                    var listItem = document.createElement('li');
+                    listItem.textContent = 'UV Index: ' + data.current.uvi;
+                    city.appendChild(listItem);
+                });
+        });
+};        
+
+   
+
+//searchButton.addEventListener('click', getSearchValue);
+    //searchButton.addEventListener('click', getApi);
+
+
 
 // fetch('https://api.github.com/orgs/twitter/public_members')
 //   .then(function (response) {
